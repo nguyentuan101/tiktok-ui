dc443f88c3d047e1beb335576d13c4b34cad5ed0
nguyentuan101 <nguyenhtuan101@gmail.com>
2022-10-05 16:06:16 +0700

    create

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Hello anh em</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+     
     </div>
   );
 }
